573a539231fb5ff9ecab75ebf575c530b170d4b9
dci <rudeshdhote@gmail.com>
2021-04-01 09:57:17 +0200

    initial commit

---
+++ start.js
@@ -0,0 +1,16 @@
+let introSentence = "hello, my name is Fran and I am ";
+let age = 25 ;
+
+console.log(introSentence + age)
+
+// + is concatenation two variables here
+
+var text = "1005";
+var integer = parseInt(text, 10);
+
+console.log(integer);
+
+var text = "10.05";
+var integer = parseFloat(text, 10);
+
+console.log(integer);
